6eaad32a17f53942ab97458633f3b3c538e38bb0
Nicolas Deleasa <nicktakesmail@gmail.com>
2025-06-16 11:17:28 -0400

    Initial Commit

---
+++ Commas/CommasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommasApp.swift
+//  Commas
+//
+//  Created by Nicolas Deleasa on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CommasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
